e9317f9d99b8bddfc05f5291f193b1e86f0502c2
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-08 18:47:22 -0600

    feat: set props for btn component, created conditional for message and btn useState

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,9 +1,31 @@
 import Card from "./Card";
 import {useState} from "react";
+import Button from "./Button";
 
 function ReviewForm() {
 
-    const [text, setText] = useState();
+    const [text, setText] = useState("");
+    const [message, setMessage] = useState(null);
+    const [btnDisabled, setBtnDisabled] = useState(true);
+
+
+
+    const handleMessage = (e) => {
+        if (text === "") {
+            setBtnDisabled(true);
+            setMessage(null);
+        } else if (text !== "" && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage( <div className="message">
+                Review must be at least 10 characters
+                </div>
+            )
+        } else {
+            setMessage(null);
+            setBtnDisabled(false)
+        }
+        setText(e.target.value)
+    }
 
     return (
         <Card>
@@ -12,11 +34,12 @@ function ReviewForm() {
             </div>
             <form className="form-wrapper">
                 <div className="form-border">
-                    <input value={text} type="text" placeholder="Write a review" required className="input"/>
-                    <button type="submit" className="btn">Submit</button>
+                    <input onChange={handleMessage} value={text} type="text" placeholder="Write a review" required className="input"/>
+                    <Button type="submit" version='secondary'  isDisabled={btnDisabled}
+                    >Submit</Button>
                 </div>
             </form>
-
+                {message !== "" ? <div> {message} </div> : ""}
         </Card>
     )
 }
