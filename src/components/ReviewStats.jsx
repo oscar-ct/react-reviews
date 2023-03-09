7cfd8052dbdd1ca21b6dc4132ab750e8e049eab1
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-08 20:47:43 -0600

    feat: created stats component, set up context

---
+++ src/components/ReviewStats.jsx
@@ -0,0 +1,18 @@
+import ReviewContext from "../context/ReviewsContext";
+import {useContext} from "react";
+
+function ReviewStats () {
+
+    const { reviews } = useContext(ReviewContext);
+
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ReviewStats;
