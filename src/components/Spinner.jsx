fde9a47ea81651c110c24ecc3d0fa37f64abf227
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-09 23:17:25 -0600

    feat: created Spinner component, added spinner gif

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+import spinner from '../spinner2.gif'
+
+function Spinner() {
+    return <img src={spinner} alt="Loading..." style={{width: "100px", margin: "auto", display: "block"}}/>
+}
+
+export default Spinner;
