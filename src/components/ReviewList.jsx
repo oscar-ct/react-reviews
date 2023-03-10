610ab40228fe90905b4cf2379200a663a2630ba2
Oscar Castro <oscar.a.castro818@gmail.com>
2023-03-09 22:58:55 -0600

    feat: set conditional for loading data from json server

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -5,25 +5,30 @@ import {motion, AnimatePresence} from "framer-motion";
 
 function ReviewList() {
 
-    const { reviews } = useContext(ReviewContext);
+    const { reviews, isLoading } = useContext(ReviewContext);
 
-    if (!reviews || reviews.length === 0) {
+    if (!isLoading && (!reviews || reviews.length === 0)) {
         return <div>Be the first to review!</div>
     }
-    return (
-        <div>
-            <AnimatePresence>
-            {reviews.map(function (review) {
-                return (
-                    <motion.div key={review.id} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
-                        <ReviewItem key={review.id} item={review}/>
-                    </motion.div>
-                )
-            })}
-            </AnimatePresence>
-        </div>
 
-    )
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    } else {
+        return (
+            <div>
+                <AnimatePresence>
+                {reviews.map(function (review) {
+                    return (
+                        <motion.div key={review.id} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+                            <ReviewItem key={review.id} item={review}/>
+                        </motion.div>
+                    )
+                })}
+                </AnimatePresence>
+            </div>
+
+        )
+    }
 
 }
 
